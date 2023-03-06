59289042f892f588348d596c296fea75460116e4
Dulgisng <dulguunsandagsuren@yahoo.com>
2023-03-06 21:50:25 +0000

    Update

--- assets/javascript/leaderboard.js
+++ assets/javascript/leaderboard.js
@@ -1,10 +1,15 @@
 const highScoresList = document.querySelector('#highScoresList')
-const highScores = JSON.parse(localStorage.getItem('highScores')) ||[]
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const userScore = JSON.parse(localStorage.getItem('userScore')) || {}
 
+// Check if the user has a score to add to the leaderboard
+if (Object.keys(userScore).length !== 0) {
+  highScores.push(userScore)
+  localStorage.setItem('highScores', JSON.stringify(highScores))
+}
 
-
-highScoresList.innerHTML =
-highScores.map(score => {
-    return`<li class="high-scores">${score.name} - ${score.score}</li>
-`
-}).join('')
+highScoresList.innerHTML = highScores
+  .sort((a, b) => b.score - a.score)
+  .map(score => {
+    return `<li class="high-scores">${score.name} - ${score.score}</li>`
+  }).join('')
